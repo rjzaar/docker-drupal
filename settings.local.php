d4cea232a9d00be2ad2ce4867b1d27c579cf69e7
Rob Zaar <rob@cathnet.org>
2017-10-31 17:54:53 +1100

    Create settings.local.php

---
+++ settings.local.php
@@ -0,0 +1,14 @@
+<?php
+
+$settings['install_profile'] = 'social';
+$settings['file_private_path'] =  '/var/www/files_private';
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\Core\Database\Driver\mysql',
+  'driver' => 'mysql',
+);
